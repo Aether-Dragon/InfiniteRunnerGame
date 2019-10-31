a9a6d9f599c6b64d2ba867a59f8393fdf7ffed33
Aether_Dragon <gr.huang207@gmail.com>
2019-10-30 17:53:00 -0700

    Added sprite classes

---
+++ core/src/com/missionbit/game/sprites/Cupcake.java
@@ -0,0 +1,61 @@
+package com.missionbit.game.sprites;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+public class Cupcake {
+    public final static int GRAVTIY = -10;
+    public final static int MOVEMENT = 50;
+    private Vector3 position;
+    private Vector3 velocity;
+    private Rectangle bounds;
+    private Animation cupcakeAnimation;
+    private Texture texture;
+
+    public Cupcake(int x, int y ) {
+        position = new Vector3(x, y, 0);
+        velocity = new Vector3(0, 0, 0);
+        texture = new Texture("cupcakeHolder.png");
+        cupcakeAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
+
+    }
+
+    public void update(float dt) {
+        cupcakeAnimation.update(dt);
+
+        if(position.y > 20) {
+            velocity.add(0, GRAVTIY, 0);
+        }
+        velocity.scl(dt);
+        position.add(MOVEMENT * dt, velocity.y, 0);
+        if(position.y < 0) {
+            position.y = 0;
+        }
+
+        velocity.scl(1/dt);
+
+        bounds.setPosition(position.x, position.y);
+
+    }
+
+    public Vector3 getPosition() { return position; }
+
+    public TextureRegion getTexture() { return cupcakeAnimation.getFrame(); }
+
+    public void jump() {
+        velocity.y = 200;
+    }
+
+    public Rectangle getBounds() { return bounds; }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+}
---
+++ core/src/com/missionbit/game/sprites/Puddle.java
@@ -0,0 +1,9 @@
+package com.missionbit.game.sprites;
+
+public class Puddle {
+    public static final int PUDDLES = 4;
+    public static final int GAP_MIN = 100;
+    public static final int GAP_MAX = 300;
+
+
+}

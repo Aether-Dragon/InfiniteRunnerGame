e65fc195a0c1dea108611e1a12f0d635ce705b98
missionbit <aiturgunbekova@s.sfusd.edu>
2019-11-13 17:58:27 -0800

    I added a cupcake and puddle animations.

--- core/src/com/missionbit/game/sprites/Bird.java
+++ core/src/com/missionbit/game/sprites/Bird.java
@@ -9,7 +9,7 @@ public class Bird {
     public static final int GAP_MIN = 100;
     public static final int GAP_MAX = 300;
     private Texture texture;
-    private Rectangle bounds ;
+    private Rectangle bounds;
     private Animation birdAnimation;
 
     public Bird(int x, int y) {
@@ -17,13 +17,17 @@ public class Bird {
         birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
         bounds = new Rectangle(x, y, birdAnimation.getFrame().getRegionHeight(), texture.getHeight());
 
-        public void update(float dt){
-            birdAnimation.update(dt);
-        }
+    }
+
+    public void update(float dt) {
+        birdAnimation.update(dt);
+    }
 
 
-        public TextureRegion getTexture() {
-            return birdAnimation.getFrame();
-        }
+    public TextureRegion getTexture() {
+        return birdAnimation.getFrame();
+
 
     }
+
+}
